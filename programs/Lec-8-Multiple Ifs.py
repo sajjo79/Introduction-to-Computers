6580c914b19baa329e93f8048eb0e8805564e050
Sajid Iqbal <sajidiqbal.pk@gmail.com>
2020-11-08 16:02:45 +0500

    Add files via upload

---
+++ programs/Lec-8-Multiple Ifs.py
@@ -0,0 +1,25 @@
+# Multiple if statements in a program
+# Reading a number from user
+"""
+a=20
+x=input("Enter the value for x:")
+y=input("Enter the value of y:")
+print(x)
+print(y)
+print(x+y) #concatenation
+"""
+
+x=int(input("Enter the value for x:"))
+y=int(input("Enter the value of y:"))
+print(x)
+print(y)
+print(x+y)
+
+"""
+# Multiple if statements
+a,b=10,20
+if a<15:
+    print("a is less than 15")
+if b<25:
+    print("b is less than 25")
+"""
---
+++ programs/Lec-8-if-statement.py
@@ -0,0 +1,39 @@
+a=10
+b=20
+if a<b:
+    print("a is less than b")   # write this statement by pressing tab
+#===================================
+if a<b:
+    print("a is less than b")
+print("this is after the if scope")
+#===================================
+a=30
+if a<b:
+    print("a is not less than b")
+print("this is after the if scope")
+#====================================
+a=10
+if a<b:
+    print("a is not less than b")
+    print("second statement in if scope")
+print("this is after the if scope")
+#====================================
+a=30
+if a<b:
+    print("a is not less than b")
+    print("second statement in if scope")
+print("this is after the if scope")
+#====================================
+a=10
+if a<b:
+    print("a is not less than b")
+    print("second statement in if scope")
+    print("3rd statement in if scope")
+print("this is after the if scope")
+#====================================
+a=30
+if a<b:
+    print("a is not less than b")
+    print("second statement in if scope")
+    print("3rd statement in if scope")
+print("this is after the if scope")
