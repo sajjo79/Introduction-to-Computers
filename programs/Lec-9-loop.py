7f4d6bf088c7b95cf39efcea7541d5baf1454493
Sajid Iqbal <sajidiqbal.pk@gmail.com>
2021-01-01 14:55:09 +0500

    Add files via upload

---
+++ programs/Lec-9-loop.py
@@ -0,0 +1,19 @@
+"""
+mycourse="Introduction to Computers"
+count=0
+while(count<4):
+    print(mycourse)
+    count=count+1
+
+i=0
+sum=0
+while(i<10):
+    sum=sum+i
+    i=i+1
+print(sum)
+"""
+i=1
+num=3
+while(i<=10):
+    print("3*"+str(i)+"=",num*i)
+    i=i+1
